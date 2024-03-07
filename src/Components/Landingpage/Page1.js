6f748178ca9ac12d613a59848d18af025e9e75ae
kudaT <timilehinkudaisi01@gmail.com>
2024-03-07 20:15:08 +0100

    Navbar and Page1 desktop design

---
+++ src/Components/Landingpage/Page1.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Page1.css";
+import { Link } from "react-router-dom";
+
+const Page1 = () => {
+  return (
+    <div>
+      <div className="div01"></div>
+    </div>
+  );
+};
+
+export default Page1;
---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,53 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "./Navbar.css";
+import logo from "../img/BENTRACO (13) 1.svg";
+import "./Navbar.css";
+import hamburger from "../img/tabler_menu-2.svg";
+import cancel from "../img/iconoir_cancel.svg";
+
+const Navbar = () => {
+  const [expand, setExpand] = useState(true);
+
+  function handler() {
+    setExpand(!expand);
+  }
+  return (
+    <div>
+      <div className="navBar">
+        <Link to="/" className="link">
+          <h1 className="carlosHead">
+            <span>CARLOS</span> CONSTRUCTION
+          </h1>
+        </Link>
+        <div className={expand ? "navs" : "navs navs-NX"}>
+          <Link to="/" className="link" onClick={handler}>
+            <h2 className="navTxt">Home</h2>
+          </Link>
+          <Link to="/services" className="link" onClick={handler}>
+            <h2 className="navTxt">Services</h2>
+          </Link>
+          <Link to="/contact" className="link" onClick={handler}>
+            <h2 className="navTxt">Contact Us</h2>
+          </Link>
+          <Link to="/about" className="link" onClick={handler}>
+            <h2 className="navTxt">About Us</h2>
+          </Link>
+          <Link to="/projects" className="link" onClick={handler}>
+            <h2 className="navTxt projects">PROJECTS</h2>
+          </Link>
+          {/* <Link to="/booking" className="link" onClick={handler}>
+            <button className="btnBook">PROJECTS</button>
+          </Link> */}
+
+          <img src={cancel} alt="" id="cancel" onClick={handler} />
+        </div>
+        <div className="hamburger">
+          <img src={hamburger} alt="" className="ham" onClick={handler} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
