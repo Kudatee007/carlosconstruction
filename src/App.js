b6386be452e2941657e8545bc0b063ff8e7b467b
kudaT <timilehinkudaisi01@gmail.com>
2024-03-09 17:06:57 +0100

    update changes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
 import Navbar from "./Components/Navbar/Navbar";
-import { BrowserRouter as Router } from 'react-router-dom';
+import About from "./Components/About Us/About";
+import Contact from "./Components/Contact/Contact";
+import Services from "./Components/Services/Services";
+import Project from "./Components/Project/Project";
 import Page1 from "./Components/Landingpage/Page1";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-     <Router>
-      <Navbar />
-      <Page1 />
-    </Router>
+      <BrowserRouter>
+      <Navbar/>
+        <Routes>
+          <Route path="/services" element={<Services />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/projects" element={<Project />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
