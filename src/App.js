61a926dcda7425695fe063cb7c70557e18e3361b
kudaT <timilehinkudaisi01@gmail.com>
2024-03-09 23:01:52 +0100

    Contact-us page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
 import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import Navbar from "./Components/Navbar/Navbar";
-import About from "./Components/About Us/About";
-import Contact from "./Components/Contact/Contact";
 import Services from "./Components/Services/Services";
+import Contact from "./Components/Contact/Contact";
+import About from "./Components/About Us/About";
 import Project from "./Components/Project/Project";
 import Page1 from "./Components/Landingpage/Page1";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./Components/Layout";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-      <Navbar/>
-        <Routes>
-          <Route path="/services" element={<Services />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/projects" element={<Project />} />
-        </Routes>
+        <Layout>
+          <Routes>
+          <Route path="/" element={<Page1 />} />
+            <Route path="/services" element={<Services />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/projects" element={<Project />} />
+          </Routes>
+        </Layout>
       </BrowserRouter>
     </div>
   );
