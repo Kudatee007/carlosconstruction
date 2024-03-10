417696605825d611f7849f94f78a634fdd431e69
kudaT <timilehinkudaisi01@gmail.com>
2024-03-10 11:33:25 +0100

    Update design changes

--- src/Components/Landingpage/Page1.js
+++ src/Components/Landingpage/Page1.js
@@ -1,11 +1,23 @@
 import React from "react";
 import "./Page1.css";
 // import { Link } from "react-router-dom";
+import videoBg from "../Images/1104411_1080p_4k_2k_3840x2160.mov";
+import carlosLogo from "../Images/carlos.svg";
 
 const Page1 = () => {
   return (
     <div>
-      <div className="div01"></div>
+      <div className="div01">
+        <video src={videoBg} autoPlay loop muted />
+        <div className="div01Txt">
+          {/* <img src={carlosLogo} alt="" className="carlosLogo" /> */}
+          <h1 className="ccTxt">CARLOS CONSTRUCTION</h1>
+          <h1 className="craftTxt">Crafting Your Vision into Reality</h1>
+          <h2 className="beyondTxt">
+            Carlos Construction Builds Beyond Expectations!
+          </h2>
+        </div>
+      </div>
     </div>
   );
 };
--- src/Components/Layout.js
+++ src/Components/Layout.js
@@ -1,9 +1,10 @@
 import React from "react";
 import Navbar from "./Navbar/Navbar";
+import "./Layout.css";
 
 const Layout = ({ children }) => {
   return (
-    <div>
+    <div className="layout-container">
       <Navbar />
       {children}
     </div>
