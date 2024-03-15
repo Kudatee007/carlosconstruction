cc1ca9e33080ef398decc529ab7ae824b21fe269
Kelvin Osuere <osuerekelvin@yahoo.com>
2024-03-15 14:36:34 +0100

    contact us edited

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "./Footer.css"
+import "./Footer.css";
 
 const Footer = () => {
   return (
@@ -7,12 +7,14 @@ const Footer = () => {
       <div className="footerDiv">
         <div>
           <h3 className="footerH3">ABOUT US</h3>
-          <h5 className="footerH5">CARLOS CONSTRUCTION</h5>
+          {/* <h5 className="footerH5">CARLOS CONSTRUCTION</h5> */}
           <p className="footerP">
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ut
-            consequatur nam vel harum! Odio vero odit deleniti fuga voluptatibus
-            harum sequi beatae distinctio! Veritatis rem pariatur fuga vero,
-            adipisci officiis!
+            <span className="carl2">Carlos Construction</span> is a renowned construction company dedicated to
+            delivering excellence in residential, commercial, and industrial
+            projects. With a legacy of craftsmanship, innovation, and client
+            satisfaction, we have established ourselves as a trusted partner for
+            construction solutions tailored to meet the diverse needs of our
+            clients.
           </p>
         </div>
         <div>
@@ -26,35 +28,37 @@ const Footer = () => {
           </ul>
         </div>
         <div>
-            <h3 className="footerH3">WORKING HOUR</h3>
-            <div className="hourDiv">
-                <p className="hourDays">Monday</p>
-                <p className="hourTime">8 am - 17 pm</p>
-            </div>
-            <div className="hourDiv">
-                <p className="hourDays">Tuesday</p>
-                <p className="hourTime">8 am - 17 pm</p>
-            </div>
-            <div className="hourDiv">
-                <p className="hourDays">Wednesday</p>
-                <p className="hourTime">8 am - 17 pm</p>
-            </div>
-            <div className="hourDiv">
-                <p className="hourDays">Thursday</p>
-                <p className="hourTime">8 am - 17 pm</p>
-            </div>
-            <div className="hourDiv">
-                <p className="hourDays">Friday</p>
-                <p className="hourTime">8 am - 17 pm</p>
-            </div>
-            <div className="hourDiv">
-                <p className="hourDays">Saturday</p>
-                <p className="hourTime">8 am - 17 pm</p>
-            </div>
+          <h3 className="footerH3">WORKING HOUR</h3>
+          <div className="hourDiv">
+            <p className="hourDays">Monday</p>
+            <p className="hourTime">9am - 5pm</p>
+          </div>
+          <div className="hourDiv">
+            <p className="hourDays">Tuesday</p>
+            <p className="hourTime">9am - 5pm</p>
+          </div>
+          <div className="hourDiv">
+            <p className="hourDays">Wednesday</p>
+            <p className="hourTime">9am - 5pm</p>
+          </div>
+          <div className="hourDiv">
+            <p className="hourDays">Thursday</p>
+            <p className="hourTime">9am - 5pm</p>
+          </div>
+          <div className="hourDiv">
+            <p className="hourDays">Friday</p>
+            <p className="hourTime">9am - 5pm</p>
+          </div>
+          <div className="hourDiv">
+            <p className="hourDays">Saturday</p>
+            <p className="hourTime">10am - 2pm</p>
+          </div>
         </div>
       </div>
       <div className="footerReserved">
-        <p className="reservedTxt">carlos construction | © 2014 constuction, All rights reserved</p>
+        <p className="reservedTxt">
+          carlos construction | © 2014 constuction, All rights reserved
+        </p>
       </div>
     </div>
   );
